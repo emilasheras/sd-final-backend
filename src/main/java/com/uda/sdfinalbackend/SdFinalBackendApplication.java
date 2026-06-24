994737bc17ea2a5740b4c0466066c9a144bccd4c
emilasheras <emperorbfd@gmail.com>
2026-06-24 00:15:24 -0300

    Spring Boot boilerplate

---
+++ src/main/java/com/uda/sdfinalbackend/SdFinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.uda.sdfinalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdFinalBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdFinalBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uda/sdfinalbackend/SdFinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uda.sdfinalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdFinalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
